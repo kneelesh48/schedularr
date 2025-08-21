becda77113a970e4d4a6d14038ad2264312c7cd0
Neelesh Kumar <kneelesh48@gmail.com>
2025-08-21 16:57:54 +0530

    Format useAuth.ts

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,11 +1,11 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import { AuthContext } from '../context/AuthContextDefinition';
+import { AuthContext } from "../context/AuthContextDefinition";
 
 export function useAuth() {
-    const context = useContext(AuthContext);
-    if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider');
-    }
-    return context;
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
 }
