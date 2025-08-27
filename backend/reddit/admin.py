615c4f150771fb6c39601a962cf53125a231364d
Neelesh Kumar <kneelesh48@gmail.com>
2025-08-28 01:19:23 +0530

    Register model admins for RedditAccount, ScheduledPost, and SubmittedPost in admin.py

--- backend/reddit/admin.py
+++ backend/reddit/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import RedditAccount, ScheduledPost, SubmittedPost
 
+
+class RedditAccountAdmin(admin.ModelAdmin):
+    list_display = ("reddit_username", "user", "reddit_account_status")
+    list_filter = ("reddit_account_status",)
+
+class ScheduledPostAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "reddit_account", "subreddit", "title", "status", "next_run")
+
+class SubmittedPostAdmin(admin.ModelAdmin):
+    list_display = ("reddit_post_id", "scheduled_post", "reddit_url", "submitted_at")
+
 # Register your models here.
-admin.site.register(RedditAccount)
-admin.site.register(ScheduledPost)
-admin.site.register(SubmittedPost)
+admin.site.register(RedditAccount, RedditAccountAdmin)
+admin.site.register(ScheduledPost, ScheduledPostAdmin)
+admin.site.register(SubmittedPost, SubmittedPostAdmin)
